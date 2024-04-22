af54d838c42544da0b5afc21b4ca33f99e672936
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-04-22 18:52:42 +0300

    Add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 // const tinyMorgan = morgan('tiny')
@@ -103,7 +105,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newContact)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
